74eb3a115102a804334cc6ef9b3ed011d8f8c2e5
hella753 <kristi.gaphrindashvili@gmail.com>
2024-10-31 19:09:07 +0400

    changed something in signals.py

--- user/signals.py
+++ user/signals.py
@@ -6,8 +6,18 @@ from order.models import Cart
 
 @receiver(post_save, sender=User)
 def create_cart_for_user(sender, **kwargs):
+    """
+    This signal checks if the user is already registered
+    on the website and if not creates a cart for the user.
+    """
     user_obj = kwargs.get("instance")
-    if not user_obj.is_superuser:
+
+    try:
+        has_cart = user_obj.cart
+    except:
+        has_cart = False
+
+    if not user_obj.is_superuser and not has_cart:
         cart = Cart(user=user_obj, flat_rate=5)
         cart.save()
         print(f"cart has been created {user_obj}")
